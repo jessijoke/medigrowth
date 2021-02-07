1cbc3fa5e7f62742e268c4f948fe843fc0d289f3
jessijoke <jessijoke@yahoo.com>
2021-02-07 16:19:41 -0500

    Styled index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,48 +9,20 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-  get "/" do
-    erb :index
-  end
-
-  get "/signup" do
-    erb :"/users/signup"
-  end
-
-  post "/signup" do
-    if params[:username] == "" || params[:password] == ""
-      redirect 'failure'
-    else
-      User.create(username: params[:username], email: params[:email], password: params[:password])
-      redirect "/login"
-    end
-  end
-
-  get "/login" do
-    erb :"/users/login"
-  end
-
-  post "/login" do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "/account"
-    else
-      redirect to "/failure"
-    end
-  end
-
   get "/failure" do
     erb :failure
   end
   
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :"users/account"
-  end
 
-  get '/logout' do
-    session.clear
-    redirect '/'
+  helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
   end
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,55 @@
+require './config/environment'
+
+class UsersController < ApplicationController
+
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    if !logged_in?
+        erb :"/users/signup"
+    else
+        redirect to '/account'
+    end
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect 'failure'
+    else
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect "/login"
+    end
+  end
+
+  get "/login" do
+    if !logged_in?
+        erb :"/users/login"
+    else
+        redirect to '/account'
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
+  end
+
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :"users/account"
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+end
