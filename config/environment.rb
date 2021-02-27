321cac4cb6001f178c55857040eaa303196a4cd6
jessijoke <jessijoke@yahoo.com>
2021-02-27 01:37:06 -0500

    Another attempt at Heroku Config

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,26 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 require 'sinatra/flash'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require(:default)
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do
+    set :database, { adapter: 'sqlite3', database: 'db/development.sqlite3' }
+    set :show_exceptions, true
+  end
+  
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/medigrowth')
+  
+    ActiveRecord::Base.establish_connection(
+      adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    )
+  end
 
 require './app/controllers/application_controller'
 require './app/controllers/doctor_controller'
